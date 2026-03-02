ae23faf0132f80b4a32319b8835bedbbd59f151a
lizhenyu <2945659544@qq.com>
2026-03-02 13:48:20 +0800

    主题选择

--- FADemo/ViewModels/SettingsPageViewModel.cs
+++ FADemo/ViewModels/SettingsPageViewModel.cs
@@ -1,3 +1,8 @@
+using Avalonia;
+using Avalonia.Media;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FluentAvalonia.Styling;
 using LyuExtensions.Aspects;
 
 namespace FADemo.ViewModels;
@@ -5,5 +10,50 @@ namespace FADemo.ViewModels;
 [Singleton]
 public partial class SettingsPageViewModel : ViewModelBase
 {
+    private readonly FluentAvaloniaTheme _faTheme;
 
+    public SettingsPageViewModel()
+    {
+        _faTheme = (Application.Current?.Styles[0] as FluentAvaloniaTheme)!;
+        CurrentAppTheme = AppThemes[0];
+        
+        if (_faTheme?.TryGetResource("SystemAccentColor", null, out var currentColor) == true)
+        {
+            CustomAccentColor = (Color)currentColor;
+        }
+    }
+
+    public ThemeVariant[] AppThemes { get; } =
+        [
+            ThemeVariant.Default,
+            ThemeVariant.Light,
+            ThemeVariant.Dark,
+        ];
+
+    [ObservableProperty] 
+    public partial ThemeVariant? CurrentAppTheme { get; set; }
+
+    partial void OnCurrentAppThemeChanged(ThemeVariant? value)
+    {
+        if (Application.Current != null)
+        {
+            Application.Current.RequestedThemeVariant = value;
+            if (value != ThemeVariant.Default)
+            {
+                _faTheme.PreferSystemTheme = false;
+            }
+            else
+            {
+                _faTheme.PreferSystemTheme = true;
+            }
+        }
+    }
+
+    [ObservableProperty] 
+    public partial Color CustomAccentColor { get; set; }
+
+    partial void OnCustomAccentColorChanged(Color value)
+    {
+        _faTheme.CustomAccentColor = value;
+    }
 }
